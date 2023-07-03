265962139372f3c5ba983faadd97d380eba4bc2a
Lucas Meyer <lucas.meyer@microsoft.com>
2023-07-03 15:38:38 -0700

    improved filename

--- toolbox/logging.py
+++ toolbox/logging.py
@@ -2,10 +2,10 @@ import os
 import logging
 
 def initialize_logger(program_name : str = __name__ ) -> logging.Logger:
-    if program_name == "___main__":
-        # get script filename
-        program_name = os.path.basename(__file__).split('.')[0]
-        
+    if program_name == "___main__":      
+        # get the filename of the main script that is calling this function
+        program_name = os.path.basename(__import__("__main__").__file__)
+
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(program_name)
 
